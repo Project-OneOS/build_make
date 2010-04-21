189302b0a41a7c11d95b8b418359ab45b00066c8
Steve Kondik <shade@chemlab.org>
2010-04-21 11:39:48 -0400

    Allow override of device asserts, including multi-device support.
    Set in board file with TARGET_OTA_ASSERT_DEVICE.
    (cherry-picked from commit 0f452f21fc9323b9d1fe746161761cf40aaa5030)
    
    Change-Id: I3d06bdc0e3e26bde0c0e646accd050364f9713b9
    
    ota_from_target_files: Remove device dependent arguments
    
    These device-specific arguments are defined at build time and are
    necessary to generate the zip correctly. Don't use command line
    arguments to specify them, but write all the needed information
    in misc_info.txt when the target-files zip is generated.
    ota_from_target_files will then read misc_info.txt and set
    everything automatically.
    
    Change-Id: Ibdbca575b76eb07b53fccfcea52a351c7e333f91
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -390,10 +390,9 @@ class BuildInfo(object):
           "system_other"] = self._partition_fingerprints["system"]
 
     # These two should be computed only after setting self._oem_props.
-    if override_device == "auto":
+    self._device = info_dict.get("ota_override_device")
+    if not self._device:
       self._device = self.GetOemProperty("ro.product.device")
-    else:
-      self._device = OPTIONS.override_device
     self._fingerprint = self.CalculateFingerprint()
     check_fingerprint(self._fingerprint)
 
