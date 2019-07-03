DirtyUnicorns/android_build

Build: Include BoardConfigQcom
Change-Id: I920d6cf76c1ab0cbe3e4b94c45b0a938147eff16

core/config.mk
@@ -220,6 +220,10 @@ endif
 # are specific to the user's build configuration.
 include $(BUILD_SYSTEM)/envsetup.mk
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include $(TOPDIR)vendor/extras/build/core/BoardConfigQcom.mk
+endif
+
 # Pruned directory options used when using findleaves.py
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)

Project-OneOS/build_make

Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

core/config.mk
@@ -220,6 +220,10 @@ endif
 # are specific to the user's build configuration.
 include $(BUILD_SYSTEM)/envsetup.mk
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include $(TOPDIR)vendor/caf/build/core/BoardConfigQcom.mk
+endif
+
 # Pruned directory options used when using findleaves.py
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)