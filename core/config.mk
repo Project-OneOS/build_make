8fe1c6a01723287b73a8385d07e5daacc8291ebb
IPSBHANGU <inderpreetbhangu2@gmail.com>
2020-03-12 12:11:29 +0530

    make: Include Pathmaps

--- core/config.mk
+++ core/config.mk
@@ -237,7 +237,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-include vendor/one/config/BoardConfigCaf.mk
+include vendor/one/config/BoardConfigOne.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
