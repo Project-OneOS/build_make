62f9549de4a00013997e33ca93cb05809033bade
IPSBHANGU <inderpreetbhangu2@gmail.com>
2020-06-26 11:00:06 +0530

    Bump Security Patch and whitelist net.oneplus.**

--- core/config.mk
+++ core/config.mk
@@ -1178,4 +1178,10 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+ifneq ($(wildcard device/one/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/one/sepolicy/common/sepolicy.mk)
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
