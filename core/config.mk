StatiXOS/android_build

make: we should include some policy in the case that we're doing a real build
Signed-off-by: Stefen Wakefield <xstefen@gmail.com>

Change-Id: Iede5830093497abe753a34df3bc4913468be39d2

core/config.mk
@@ -1144,6 +1144,12 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+ifneq ($(STATIX_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/statix/sepolicy/common/sepolicy.mk)
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk

Project-OneOS/build_make

core/config.mk
@@ -1252,4 +1252,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 # Make RECORD_ALL_DEPS readonly and also set it if deps-license is a goal.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))$(filter deps-license,$(MAKECMDGOALS))
 
+ifneq ($(ONE_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/one/sepolicy/common/sepolicy.mk)
+endif
+
+
 include $(BUILD_SYSTEM)/dumpvar.mk