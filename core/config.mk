StatiXOS/android_build

[DNM] build: switch to if exists include for sepolicy
* we don't need this for bringup just yet

core/config.mk
@@ -1248,7 +1248,7 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 ifneq ($(STATIX_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/statix/sepolicy/common/sepolicy.mk)
+$(call inherit-product-if-exists, device/statix/sepolicy/common/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk

Project-OneOS/build_make

Change-Id: Ie7c098862db25937bc88791c4bab1993f111aad4

core/config.mk
@@ -1255,7 +1255,7 @@ RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))$(filter deps-license,$(MA
 ifneq ($(ONE_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/one/sepolicy/common/sepolicy.mk)
+$(call inherit-product-if-exists, device/one/sepolicy/common/sepolicy.mk)
 endif
 
 