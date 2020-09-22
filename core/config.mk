StatiXOS/android_build

config: Include custom boardconfig a bit later
- We're trying to export MAKE_PREBUILT to soong
- Let's include it right before soong_config.mk is included

*edits
- remove pathmap include, this has moved

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

core/config.mk
@@ -226,10 +226,6 @@ endif
 # are specific to the user's build configuration.
 include $(BUILD_SYSTEM)/envsetup.mk
 
-ifneq ($(STATIX_BUILD),)
-include vendor/statix/config/BoardConfigStatix.mk
-endif
-
 # Pruned directory options used when using findleaves.py
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
@@ -1174,6 +1170,10 @@ dont_bother_goals := out \
     vbmetaimage-nodeps \
     product-graph dump-products
 
+ifneq ($(STATIX_BUILD),)
+include vendor/statix/config/BoardConfigStatix.mk
+endif
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
@@ -1188,9 +1188,3 @@ include $(BUILD_SYSTEM)/dumpvar.mk
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
-
-# QCOM targets and pathmap
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include $(TOPDIR)vendor/statix/build/core/pathmap.mk
-include $(TOPDIR)vendor/statix/build/core/qcom_target.mk
-endif

core/pathmap.mk
@@ -93,3 +93,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+include $(TOPDIR)vendor/statix/build/core/pathmap.mk

Project-OneOS/build_make

Change-Id: I4498a5501b5adaaffbfdcecf0fc2af5fa480c249

core/config.mk
@@ -290,10 +290,6 @@ endif
 # are specific to the user's build configuration.
 include $(BUILD_SYSTEM)/envsetup.mk
 
-ifneq ($(ONE_BUILD),)
-include vendor/one/config/BoardConfigOne.mk
-endif
-
 # Pruned directory options used when using findleaves.py
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
@@ -1231,6 +1227,10 @@ dont_bother_goals := out \
     recoveryimage-nodeps \
     vbmetaimage-nodeps \
     product-graph dump-products
+
+ifneq ($(ONE_BUILD),)
+include vendor/one/config/BoardConfigOne.mk
+endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk

core/pathmap.mk
@@ -93,3 +93,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+include $(TOPDIR)vendor/one/build/core/pathmap.mk